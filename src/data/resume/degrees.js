de5701ff1e8901bf7575beb410e1876f5aaa49d1
mdurand <72691393+Maxim-Durand@users.noreply.github.com>
2023-11-16 10:02:56 -0500

    fixed test

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -2,11 +2,13 @@ const degrees = [
   {
     school: 'INSA Rennes',
     degree: 'MS. Computer Science Engineering',
+    link: '',
     year: 2023,
   },
   {
     school: 'Ecole de Technologie Supérieure (ETS)',
     degree: 'MS. Computer Science Engineering',
+    link: '',
     year: 2023,
   },
 ];
